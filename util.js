46a5c17a60adf6dfd810a483b60d197ea5a8eb44
Tim Sampson <timsampson@gmail.com>
2020-10-17 07:25:12 +0800

    housekeeping

--- util.js
+++ util.js
@@ -6,6 +6,11 @@ const finalDir = "appsscript";
 const distDir = "dist";
 const htmlFile = "index.html";
 
+// If app is cloned from git, it won't contain a dist folder and will have an error on build.
+fs.mkdir('dist', { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
 fs.copyFileSync(path.resolve(__dirname, distDir, htmlFile), path.resolve(__dirname, finalDir, htmlFile));
 
 

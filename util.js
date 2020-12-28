d6474b184ae4a4bf87f7b98dd0ab594a7a33f21f
Tim Sampson <timsampson@gmail.com>
2020-12-28 12:31:00 +0800

    Updated build script for build folder

--- util.js
+++ util.js
@@ -6,17 +6,10 @@ const finalDir = "appsscript";
 const distDir = "dist";
 const htmlFile = "index.html";
 
-// If app is cloned from git, it won't contain a dist folder and will have an error on build.
-fs.mkdir('dist', { recursive: true }, (err) => {
-    if (err) throw err;
-});
-
 fs.copyFileSync(path.resolve(__dirname, distDir, htmlFile), path.resolve(__dirname, finalDir, htmlFile));
-
 
 const dir = fs.readdirSync(path.resolve(__dirname, servDir));
 
 dir.forEach(f => {
     fs.copyFileSync(path.resolve(__dirname, servDir, f), path.resolve(__dirname, finalDir, f));
-
 })
